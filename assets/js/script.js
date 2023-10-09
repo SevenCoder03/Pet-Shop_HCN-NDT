62db658be6d0b7c204fc40692263d273d6ed3142
SevenCoder03 <hochinhan5612@gmail.com>
2023-10-10 00:34:02 +0700

    Code brands

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,23 @@ searchInput.addEventListener("focus", () => {
 searchInput.addEventListener("blur", () => {
     searchInput.setAttribute("placeholder", "Search products...");
 });
+
+// Brands Hover
+
+const brandList = document.querySelectorAll(".brands__item");
+
+brandList.forEach((brands__item) => {
+    brands__item.addEventListener("mouseenter", () => {
+        brandList.forEach((otherBrand) => {
+            if (otherBrand !== brands__item) {
+                otherBrand.style.filter = "grayscale(100%) opacity(0.8)";
+            }
+        });
+    });
+
+    brands__item.addEventListener("mouseleave", () => {
+        brandList.forEach((otherBrand) => {
+            otherBrand.style.filter = "none";
+        });
+    });
+});
