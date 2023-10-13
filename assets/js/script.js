a15f6ec30e4ad78ac8d06a8bdf0c09be6f627fa4
SevenCoder03 <hochinhan5612@gmail.com>
2023-10-14 01:17:52 +0700

    Code login form

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,43 @@ searchInput.addEventListener("blur", () => {
     searchInput.setAttribute("placeholder", "Search products...");
 });
 
+// Login form
+
+var account = document.getElementById("account");
+var loginForm = document.getElementById("login__form");
+
+account.addEventListener("click", function () {
+    if (loginForm.style.display === "block") {
+        loginForm.style.display = "none";
+    } else {
+        loginForm.style.display = "block";
+    }
+});
+
+// Username form
+
+const unameInput = document.getElementById("uname__form");
+
+unameInput.addEventListener("focus", () => {
+    unameInput.removeAttribute("placeholder");
+});
+
+unameInput.addEventListener("blur", () => {
+    unameInput.setAttribute("placeholder", "Username");
+});
+
+// psw form
+
+const pswInput = document.getElementById("psw__form");
+
+pswInput.addEventListener("focus", () => {
+    pswInput.removeAttribute("placeholder");
+});
+
+pswInput.addEventListener("blur", () => {
+    pswInput.setAttribute("placeholder", "Password");
+});
+
 // Brands hover
 
 const brandList = document.querySelectorAll(".brands__item");
