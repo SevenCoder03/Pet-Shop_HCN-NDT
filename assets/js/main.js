dc3f8c7d800ed83e348e48532d255f5f775fd88a
SevenCoder03 <hochinhan5612@gmail.com>
2023-10-16 17:59:29 +0700

    Code my account

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,3 +70,15 @@ brandList.forEach((brands__item) => {
         });
     });
 });
+
+// Email form
+
+const emailInput = document.getElementById("email__form");
+
+emailInput.addEventListener("focus", () => {
+    emailInput.removeAttribute("placeholder");
+});
+
+emailInput.addEventListener("blur", () => {
+    emailInput.setAttribute("placeholder", "Enter your email...");
+});
