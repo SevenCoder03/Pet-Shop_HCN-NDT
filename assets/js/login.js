7dcaf500d79de1ccec452d927a6f9db793441794
SevenCoder03 <hochinhan5612@gmail.com>
2023-10-22 23:20:27 +0700

    Code eye password

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,22 +2,20 @@
 
 var account = document.getElementById("account");
 var loginForm = document.getElementById("login__form");
-var usernameInput = document.getElementById("login-form__uname");
-var passwordInput = document.getElementById("login-form__psw");
+const unameInput = document.getElementById("login-form__uname");
+const pswInput = document.getElementById("login-form__psw");
 
 account.addEventListener("click", function () {
     if (loginForm.style.display === "block") {
         loginForm.style.display = "none";
-        usernameInput.value = "";
-        passwordInput.value = "";
+        unameInput.value = "";
+        pswInput.value = "";
     } else {
         loginForm.style.display = "block";
     }
 });
 
 // Username form
-
-const unameInput = document.getElementById("login-form__uname");
 
 unameInput.addEventListener("focus", () => {
     unameInput.removeAttribute("placeholder");
@@ -29,12 +27,43 @@ unameInput.addEventListener("blur", () => {
 
 // psw form
 
-const pswInput = document.getElementById("login-form__psw");
-
 pswInput.addEventListener("focus", () => {
     pswInput.removeAttribute("placeholder");
 });
 
 pswInput.addEventListener("blur", () => {
     pswInput.setAttribute("placeholder", "Password");
+});
+
+// Eye psw
+const eyeOpen = document.querySelector(".login-form__eye-open");
+const eyeClose = document.querySelector(".login-form__eye-close");
+
+eyeOpen.addEventListener("click", function () {
+    eyeOpen.classList.add("hidden");
+    eyeClose.classList.remove("hidden");
+    pswInput.setAttribute("type", "password");
+});
+
+eyeClose.addEventListener("click", function () {
+    eyeClose.classList.add("hidden");
+    eyeOpen.classList.remove("hidden");
+    pswInput.setAttribute("type", "text");
+});
+
+// Eye psw 2
+const pswInput2 = document.querySelector(".login-form__psw-2");
+const eyeOpen2 = document.querySelector(".eye-open-2");
+const eyeClose2 = document.querySelector(".eye-close-2");
+
+eyeOpen2.addEventListener("click", function () {
+    eyeOpen2.classList.add("hidden");
+    eyeClose2.classList.remove("hidden");
+    pswInput2.setAttribute("type", "password");
+});
+
+eyeClose2.addEventListener("click", function () {
+    eyeClose2.classList.add("hidden");
+    eyeOpen2.classList.remove("hidden");
+    pswInput2.setAttribute("type", "text");
 });
